5b81c62db888f148500a605e6625c964fac4e722
AGM-33rus <rostis33@mail.ru>
2022-09-10 22:48:14 +0300

    publication_github

---
+++ javascript/src/js/index.js
@@ -0,0 +1,11 @@
+let proto = {
+  age: 25,
+};
+
+console.log(proto.age);
+
+/* let div = document.createElement("div"); */
+// div.className = "alert";
+// div.innerHTML = "<strong>Всем привет</strong>Важное сообщение";
+// document.body.append(div);
+/* alert("text"); */
